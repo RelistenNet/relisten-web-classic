32febd8c0813a8621ddb6eb8653faef0a24cba16
Daniel Saewitz <switz213@gmail.com>
2013-03-18 17:23:10 -0400

    Name in header

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -121,7 +121,9 @@ function program1(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=ul-header>Show</div>\n<ul>\n";
+  buffer += "<div class=ul-header>"
+    + escapeExpression(((stack1 = ((stack1 = depth0.shows),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div>\n<ul>\n";
   stack2 = helpers.each.call(depth0, ((stack1 = depth0.shows),stack1 == null || stack1 === false ? stack1 : stack1.children), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n</ul>\n";
@@ -148,7 +150,9 @@ function program1(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=ul-header>Songs</div>\n<ul>\n";
+  buffer += "<div class=ul-header>"
+    + escapeExpression(((stack1 = ((stack1 = depth0.songs),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</div>\n<ul>\n";
   stack2 = helpers.each.call(depth0, ((stack1 = depth0.songs),stack1 == null || stack1 === false ? stack1 : stack1.children), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n</ul>\n";
