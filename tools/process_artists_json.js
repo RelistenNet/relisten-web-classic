6c5b15a87efe32dc9b97a4989585e2f98cd872c6
Daniel Saewitz <switz213@gmail.com>
2016-04-19 17:03:33 -0400

    Add commas to numbers in show count

--- tools/process_artists_json.js
+++ tools/process_artists_json.js
@@ -3,6 +3,10 @@ function removeThe(name) {
     return name.replace(/^The /, "").trim();
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 var content = '';
 process.stdin.resume();
 process.stdin.on('data', function(buf) { content += buf.toString(); });
@@ -20,7 +24,7 @@ process.stdin.on('end', function() {
     }).sort(function (a, b) {
     	return removeThe(a.name).localeCompare(removeThe(b.name));
     }).forEach(function (v) {
-    	artists[v.slug] = {name: v.name, shows: v.shows};
+    	artists[v.slug] = {name: v.name, shows: numberWithCommas(v.shows)};
     });
 
     console.log(JSON.stringify(artists));
